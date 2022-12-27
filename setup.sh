682c8a7ce9f15236b381f5a2a7b5665d197f3aba
Richie FY <b08901039@ntu.edu.tw>
2022-12-28 02:30:36 +0800

    setup: mkdir checkpoint

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ pip3 install transformers
 pip3 install speechbrain --ignore-installed ruamel.yaml
 pip3 install gdown
 
+mkdir -p checkpoint
 gdown 1nfjQ7R2P4pc2WD3_EAOMsAyajo7gHrWg
 mv GPT2_12L_10G.zip checkpoint/
 unzip checkpoint/GPT2_12L_10G.zip -d checkpoint
